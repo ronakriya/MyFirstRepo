820027c15fbddb6f2229f6af5c5bb4326f8a5881
ronakriya <102565757+ronakriya@users.noreply.github.com>
2022-03-30 14:40:17 +0530

    by ronak
    change function name

---
+++ Addtest.java
@@ -0,0 +1,21 @@
+
+public class Addtest 
+{
+
+		int a=10; int b=30;int c;
+		void print()
+		{
+			c=a+b;
+			System.out.println(c);
+		}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+		Addtest a=new Addtest();
+		a.print();
+		}
+
+
+	
+
+}
